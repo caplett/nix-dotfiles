d24cb27b11bfed89a19066803e484bfeb71483d8
Stefan Geyer <git@stefan-geyer.org>
2021-11-13 22:57:18 +0100

    move tap enable/disable from fish alias to nix utils scripts

--- utils.nix
+++ utils.nix
@@ -11,16 +11,38 @@
         text = ''
           #! ${pkgs.bash}/bin/sh
           read lcd < /tmp/lcd
-            if [ "$lcd" -eq "0" ]; then
-                swaymsg "output * dpms on"
-                echo 1 > /tmp/lcd
-            else
-                swaymsg "output * dpms off"
-                echo 0 > /tmp/lcd
-            fi
-        '';
-      }
-    )
-  ];
+          if [ "$lcd" -eq "0" ]; then
+          swaymsg "output * dpms on"
+          echo 1 > /tmp/lcd
+          else
+          swaymsg "output * dpms off"
+          echo 0 > /tmp/lcd
+          fi
+          '';
+        }
+        )
+        (
+          pkgs.writeTextFile {
+            name = "tap_enable";
+            destination = "/bin/tap_enable";
+            executable = true;
+            text = ''
+          #! ${pkgs.bash}/bin/sh
+          swaymsg input type:touchpad tap enable
+          '';
+        }   )
+        (
+          pkgs.writeTextFile {
+            name = "tap_disable";
+            destination = "/bin/tap_disable";
+            executable = true;
+            text = ''
+          #! ${pkgs.bash}/bin/sh
+          swaymsg input type:touchpad tap disable
+          '';
+        }
 
-}
+        )
+      ];
+
+    }
