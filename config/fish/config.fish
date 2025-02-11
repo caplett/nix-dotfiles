36d57517d6cc395a6cd7f348745cd66d18f2269f
Stefan Geyer <git@stefan-geyer.org>
2025-02-11 21:35:43 +0100

    remove fzf and direnv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -58,9 +58,9 @@ if status is-interactive
         starship init fish | source
     end
 
-    if command --search direnv > /dev/null
-        eval (direnv hook fish)
-    end
+    # if command --search direnv > /dev/null
+    #     eval (direnv hook fish)
+    # end
 
     set PATH $PATH /home/stefan/.nix-profile/bin
     set PATH $PATH /nix/var/nix/profiles/default/bin
@@ -69,11 +69,14 @@ if status is-interactive
     set HYDRA_FULL_ERROR 1
     set -g -x EDITOR nvim
     set -g -x TERM xterm 
-    set -g -x FZF_ALT_C_COMMAND '/home/stefan/.nix-profile/bin/fd --type d'
+    # set -g -x FZF_ALT_C_COMMAND '/home/stefan/.nix-profile/bin/fd --type d'
+
+    # autin shell history 
+    atuin init fish | source
 
 end
 
-direnv hook fish | source
+# direnv hook fish | source
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
@@ -89,6 +92,8 @@ set -U Z_DATA_DIR /home/stefan/.local/share/z
 set -U Z_EXCLUDE '^/home/stefan$'
 set -U MOZ_DISABLE_WAYLAND_PROXY 1
 set -U __fish_initialized 3100
+set -U KUBECONFIG '/home/stefan/.kube/k3s.yaml'
+
 
 set -U _fish_abbr_s sudo -E
 
@@ -102,7 +107,7 @@ set -U _fish_abbr_gl lazygit
 set -U _fish_abbr_gs git status
 set -U _fish_abbr_g git
 set -U _fish_abbr_r ranger
-set -U _fish_abbr_kl 'ps -A | /home/stefan/.fzf/bin/fzf | grep -Eoh \"[^:][0-9]{2,6}[^:]\" | xargs kill'
+# set -U _fish_abbr_kl 'ps -A | /home/stefan/.fzf/bin/fzf | grep -Eoh \"[^:][0-9]{2,6}[^:]\" | xargs kill'
 set -U _fish_abbr_h htop
 set -U _fish_abbr_p 'ping 1.1'
 set -U _fish_abbr_pdf sioyek      
@@ -122,5 +127,7 @@ set -U _fish_abbr_l ls
 set -U _fish_abbr_ssf 'grim -g (slurp)'
 set -U _fish_abbr_ss 'grim -g (slurp) - | xclip -i -selection clipboard -t image/png'
 set -U --export nixos_fish_plugins_installed yes
-set -U fish_user_paths /home/stefan/.fzf/bin
+# set -U fish_user_paths /home/stefan/.fzf/bin
 
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
